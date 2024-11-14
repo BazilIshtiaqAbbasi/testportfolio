5b29f48de6d7cdc0b0648cbd702d46908bb22c6f
BazilIshtiaqAbbasi <bazilishtiaqabbasi@gmail.com>
2024-11-14 13:08:11 +0500

    test10

--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -102,10 +102,24 @@ export const HeroParallax = ({
 };
 
 export const Header = () => {
+  const name = "Zoha Sajid Abbasi";
+  const letters = name.split("");
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
+      <h1 className="text-3xl md:text-9xl font-bold dark:text-purple-700 mb-10">
+      {letters.map((letter, index) => (
+                  <motion.span
+                    key={index}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ delay: index * 0.1, duration: 1, ease: "backInOut" }}
+                  >
+                    {letter}
+                  </motion.span>
+                ))}
+                </h1>
       <h1 className="text-2xl md:text-8xl font-bold dark:text-white">
-        Portfolio
+        SEO Specialist
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
         We build beautiful products with the latest technologies and frameworks.
